9e06578c779ca7e26a1b040d19bcd475c3c26491
Mocuto Oshi <xmocuto@aim.com>
2016-08-29 12:15:42 -0400

    Cleaned some stuff up, commented the line where parallelization occurs

--- src/main/scala/com/mandelmoc/Hello.scala
+++ src/main/scala/com/mandelmoc/Hello.scala
@@ -14,10 +14,10 @@ object MandelMoc {
 		def recurse(x : Double, y : Double, cx : Double, cy : Double, iterations : Int) : Int = {
 			if(iterations > maxIter || math.pow(x, 2) + math.pow(y,2) >= 4.0)
 			{
-				//println(s"exiting: $x $y $iterations")
 				iterations
 			}
-			else {
+			else
+			{
 				val x1 = math.pow(x, 2) - math.pow(y, 2) + cx
 				val y1 = 2 * x * y + cy
 				recurse(x1, y1, cx, cy, iterations + 1)
@@ -27,24 +27,21 @@ object MandelMoc {
 		val img = new BufferedImage(width, width, BufferedImage.TYPE_INT_RGB)
 
 		def renderPixel(index : Int) : Unit = {
-			val x = index % width
-			val y = index / width
-			val cx = (x - (width / 2.0) )* (4.0 / width)
-			val cy = (y - (width / 2.0)) * (4.0 / width)
+			val (x, y) = (index % width, image / width)
+			val (cx, cy) = ((x - (width / 2.0) )* (4.0 / width), (y - (width / 2.0)) * (4.0 / width))
 
 			val iter = recurse(cx, cy, cx, cy, 0)
 			val color = genColor(iter, maxIter)
 			img.setRGB(x, y, color.getRGB)
 		}
 
-		(0 until (width * width)).par.foreach(i => renderPixel(i))
+		(0 until (width * width)).par.foreach(i => renderPixel(i)) // This line is where the parallelization occurs
 
 		img
 
 	}
 
 	def genColor(iter : Int, maxIter : Int) : Color = new Color(iter / maxIter * 255, iter / maxIter * 255, iter / maxIter * 255)
-	//def genColor(iter : Int, maxIter : Int) : Color = new Color(255, 255, 255)
 
 	def enterDimensions() : Int = {
 		try {
@@ -55,14 +52,8 @@ object MandelMoc {
 	    if (w > 0) w else { println("Image dimensions must be positive. Let's start from the top."); enterDimensions() }
 		}
 		catch {
-			case c : java.lang.NumberFormatException => {
-				println("Please enter a correctly formatted number. Let's start from the top.")
-				enterDimensions()
-			}
-			case _ : Throwable => {
-				println("Something went wrong, let's just start from the top")
-				enterDimensions()
-			}
+			case c : java.lang.NumberFormatException => { println("Please enter a correctly formatted number. Let's start from the top."); enterDimensions() }
+			case _ : Throwable => { println("Something went wrong, let's just start from the top"); enterDimensions() }
 		}
 	}
 
