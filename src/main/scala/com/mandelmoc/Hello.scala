a96d59fb6e31705c67d41188e86ee0246dd31c5e
Mocuto Oshi <xmocuto@aim.com>
2016-09-17 21:49:27 -0400

    Added cool colors, fixed bug

--- src/main/scala/com/mandelmoc/Hello.scala
+++ src/main/scala/com/mandelmoc/Hello.scala
@@ -9,6 +9,17 @@ import scala.annotation.tailrec
 
 object MandelMoc {
 
+	val Colors = List(
+		Color.CYAN,
+		Color.MAGENTA,
+		Color.YELLOW,
+		Color.RED,
+		Color.GREEN,
+		Color.BLUE,
+		Color.WHITE,
+		Color.BLACK
+	)
+
 	def start(width : Int, maxIter : Int) : BufferedImage = {
 		@tailrec
 		def recurse(x : Double, y : Double, cx : Double, cy : Double, iterations : Int) : Int = {
@@ -27,7 +38,7 @@ object MandelMoc {
 		val img = new BufferedImage(width, width, BufferedImage.TYPE_INT_RGB)
 
 		def renderPixel(index : Int) : Unit = {
-			val (x, y) = (index % width, image / width)
+			val (x, y) = (index % width, index / width)
 			val (cx, cy) = ((x - (width / 2.0) )* (4.0 / width), (y - (width / 2.0)) * (4.0 / width))
 
 			val iter = recurse(cx, cy, cx, cy, 0)
@@ -41,7 +52,9 @@ object MandelMoc {
 
 	}
 
-	def genColor(iter : Int, maxIter : Int) : Color = new Color(iter / maxIter * 255, iter / maxIter * 255, iter / maxIter * 255)
+	//def genColor(iter : Int, maxIter : Int) : Color = new Color(iter / maxIter * 255.0, iter / maxIter * 255.0, iter / maxIter * 255.0)
+
+	def genColor(iter : Int, maxIter : Int) : Color = Colors(iter % Colors.length)
 
 	def enterDimensions() : Int = {
 		try {
